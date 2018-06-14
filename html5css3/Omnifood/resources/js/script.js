e84026d90bea6dfc6ce987caa93f058a0c4744da
goongoguma <wogus7an@gmail.com>
2018-06-15 07:15:27 +0900

    Building-a-responsive-website Jquery and position sitcky

---
+++ html5css3/Omnifood/resources/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  /* select 'h1' element and then use 'click' method
+   so when we click h1 element, it select 'this' which is h1 element, and it changes the css style of hat element
+   and in this case we make backgound-color makes it red */
+  // $('h1').click(function() {
+  //   $(this).css('background-color', '#ff0000');
+  // })
+
+});
