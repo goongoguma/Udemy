1e35ef12435054b76b0e87d2509c585979f93c2b
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-06-26 20:57:37 +0900

    JavaScript-algorithm-quiz
    Function
    Function Statements and Expressions
    Arrays
    Objects and properties
    Objects and methods
    Continue and break statement

--- Complete JavaScript Course 2018/0626.js
+++ Complete JavaScript Course 2018/0626.js
@@ -147,3 +147,48 @@
 // console.log(john);
 
 
+/******************************************
+* Loops and Iteration
+*/
+
+// for loop
+// for(var i = 1; i <= 20; i += 2){
+//   console.log(i);
+// }
+
+// i = 0, 0 < 10 true, log i to console, i++
+// i = 1, 1, 10 true, log i to the console, i++
+//...
+// i = 9, 9 < 10 true, log i to the console, i++
+// i = 10, 10 < 10 false, exit the loop!
+
+// var john = ['John', 'Smith', 1990, 'teacher', false];
+
+// for (var i = 0; i < john.length; i++) {
+//   console.log(john[i]);
+// }
+
+// // while loop
+// var i = 0;
+// while(i < john.length) {
+//   console.log(john[i]);
+//   i++;
+// }
+
+// Continue and break statements
+// var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+// for (var i = 0; i < john.length; i++) {
+//   if(typeof john[i] !== 'string' ) continue;
+//   console.log(john[i]);
+// }
+// for (var i = 0; i < john.length; i++) {
+//   if(typeof john[i] !== 'string' ) break;
+//   console.log(john[i]);
+// }
+
+// // Looping backwards
+// var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+// for(var i = john.length - 1; i >= 0; i--){
+//   console.log(john[john.length]);
+// }
+
