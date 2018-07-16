a7db2441f1b66a964d0cd35b30e2b0a3be136e93
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-07-16 14:48:01 +0900

    Complete JavaScript Course
    Destructuring
    Arrays in ES6

---
+++ Complete JavaScript Course 2018/0716.js
@@ -0,0 +1,89 @@
+// LECTURE : Destructuring -> Convenient way extracting data from data structure
+
+// ES5
+var john = ['John', 26];
+// var name = john[0];
+// var age = john[1];
+
+
+// ES6
+const [name,year] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+  firstName: 'John',
+  lastName: 'Smith'
+}
+
+const {fristName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const{fristName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+
+function calcAgeRetirement(year) {
+  const age = new Date().getFullYear() - year;
+  return [age, 65 - age];
+}
+
+
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
+
+/////////////////////////////////////////////////////
+// LECTURE : Arrays in ES6
+
+const boxes = document.querySelectorAll('.box');
+// change 'list' to 'array'
+
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur) {
+  cur.style.backgroundColor = 'dodgerblue';
+})
+
+// ES6
+const boxesArr6 = Array.from(boxes); //'from' transforms the nodelist 'boxes' to an array
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+
+// LOOP
+
+// ES5
+for(var i = 0; i < boxesArr5.length; i++) {
+  if(boxesArr5[i].className === 'box blue') {
+    continue; // skip the iteration of the loop and goes right to the next one
+  }
+  boxesArr5[i].textContent = 'I changed to blue!';
+}
+
+// ES6
+for(const cur of boxesArr6) {
+    if(cur.className.includes('blue') === 'box blue') {
+      continue;
+    }
+    cur.textContent = 'I changed to blue!';
+}
+
+
+// FINDING ELEMENTS IN AN ARRAY
+
+// ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function(cur) {
+  return cur => 18;
+});
+console.log(full);
+
+console.log(full.indexOf(true)); // 3
+console.log(ages[full.indexOf(true)]); // 21
+
+// ES6
+console.log(ages.findIndex(cur => cur >= 18)); // 3 (value)
+console.log(ages.find(cur => cur >= 18)); // 21 (index)
