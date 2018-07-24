3e3dbfc68127d5744ddb505d992189b49f98e3d5
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-07-24 14:26:47 +0900

    Custom HTTP library Ajax with Callback

---
+++ Asynchronous JavaScript, Ajax and Fetch API/7_8_project_files/easyhttp/app.js
@@ -0,0 +1,16 @@
+const http = new easyHTTP;
+
+// // Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts');
+
+// Callback
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+{
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+}
+});
+
