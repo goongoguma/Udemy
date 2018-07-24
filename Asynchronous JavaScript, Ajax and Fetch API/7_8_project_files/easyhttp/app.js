6bfff4590781b81211821b74472778dadd71ea79
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-07-24 15:32:51 +0900

    Custom HTTP library Ajax with Callback(1)
    GET

--- Asynchronous JavaScript, Ajax and Fetch API/7_8_project_files/easyhttp/app.js
+++ Asynchronous JavaScript, Ajax and Fetch API/7_8_project_files/easyhttp/app.js
@@ -3,14 +3,27 @@ const http = new easyHTTP;
 // // Get Posts
 // http.get('https://jsonplaceholder.typicode.com/posts');
 
+// // Callback
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+// {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// }
+// });
+
+// Get Single Post
 // Callback
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
 {
   if(err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(post);
   }
 }
-});
+}); // it gives single post userId:1
+
 
