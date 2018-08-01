d4dd54e883bfaed0ad25165661154e24dee63aa6
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-08-01 15:36:18 +0900

    Complete React

--- Complete React/indecision-app/public/public/scripts/app.js
+++ Complete React/indecision-app/public/public/scripts/app.js
@@ -3,11 +3,63 @@
 console.log('App.js is running!');
 
 // JSX(JavaScript Syntax Extension) - JavaScript XML
-var template = React.createElement(
-  'h1',
+var template = // in React, elements have to be bound in one root element
+React.createElement(
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+// Challenge1 : Create a templateTwo var JSX expression using
+// div
+//  h1 -> My name
+//  p -> Age :
+//  p -> Location :
+// Render templateTwo instead of template
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    ' My Name : Jay An '
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' Age : 29 '
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' Location : Incheon '
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot); // This is where we are gonna render out application
+ReactDOM.render(templateTwo, appRoot); // This is where we are gonna render out application
