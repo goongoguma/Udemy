7e203c75006fb0f81aa9e30493c094b538fda041
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-08-02 14:35:49 +0900

    Complete React
    Exploring JSX
    JSX Expressions
    Conditional Rendering in JSX
    Events and Attributes

--- Complete React/indecision-app/public/src/app.js
+++ Complete React/indecision-app/public/src/app.js
@@ -1,38 +1,47 @@
 console.log('App.js is running!');
 
 // JSX(JavaScript Syntax Extension) - JavaScript XML
-var template = ( // in React, elements have to be bound in one root element
-<div>
-  <h1>Indecision App</h1> 
-  <p>This is some info</p>
-  <ol>
-    <li>Item one</li>
-    <li>Item two</li>
-  </ol>
-</div>
+// const template = ( // in React, elements have to be bound in one root element
+// <div>
+//   <h1>Indecision App</h1> 
+//   <p>This is some info</p>
+//   <ol>
+//     <li>Item one</li>
+//     <li>Item two</li>
+//   </ol>
+// </div>
+// );
+
+/// Events and Attributes
+// in React, className is used instead of class 
+// let count = 0;
+// const addOne = () => console.log('addOne');
+// const minusOne = () => console.log("MINUSONE");
+// const reset = () => console.log("RESET");
+
+// const templateTwo = (
+//   <div>
+//   <h1>Count: {count}</h1>
+//   <button onClick={addOne}>+1</button> 
+//   <button onClick={minusOne}>-1</button> 
+//   <button onClick={reset}>reset</button>
+//   </div>
+// );
+
+/// Manual Data Binding
+let count = 0;
+const addOne = () => console.log('addOne');
+const minusOne = () => console.log("MINUSONE");
+const reset = () => console.log("RESET");
+
+const templateTwo = (
+  <div>
+  <h1>Count: {count}</h1>
+  <button onClick={addOne}>+1</button> 
+  <button onClick={minusOne}>-1</button> 
+  <button onClick={reset}>reset</button>
+  </div>
 );
+const appRoot = document.getElementById('app');
 
-/// Challenge3 : Only render the subtitle (and p tag) if subtitle exist - logical and operator
-// render new p tag - if options.length > 0 "Here are your options" unless "No options"
-var app = {
-    title : 'Indecision App',
-    subtitle : 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
-  };
-  var template = ( 
-    <div>
-      <h1>{app.title}</h1> 
-      {app.subtitle && <p>Subtitle : {app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <ol>
-        <li>Item one</li>
-        <li>Item two</li>
-      </ol>
-    </div>
-    );
-  
- 
-  
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot); // This is where we are gonna render out application
+ReactDOM.render(templateTwo, appRoot); // This is where we are gonna render out application
