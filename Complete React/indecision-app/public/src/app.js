f469dc66a27ba983035acfd1b88897f828784d18
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-08-05 08:57:10 +0900

    Complete React
    Events and Methods

--- Complete React/indecision-app/public/src/app.js
+++ Complete React/indecision-app/public/src/app.js
@@ -24,27 +24,32 @@ class Header extends React.Component{
   }
 }
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
   render() {
     return (
       <div>
-      <button> What should I do? </button>
+      <button onClick={this.handlePick}> What should I do? </button>
       </div>
     );
   }
 }
-
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('Do you want to remove all?')
+  }
   render() {
     return (
       <div>
+      <button onClick={this.handleRemoveAll}>Remove All</button>
       {
-        this.props.options.map((option) => <Option key={option} optionText={option}/>)
+        this.props.options.map((e) => <Option key={e} optionText={e}/>)
       }
       </div>
     );
   }
 }
-
 // Option -> Option component here
 class Option extends React.Component {
   render() {
@@ -56,11 +61,25 @@ class Option extends React.Component {
   }
 }
 
+// 1. Setup the form with text input and submit button
+// 2. Wire up onSubmit
+// 3. handleAddOption -> fetch the value typed -> if value, then alert
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+   if(option) {
+    alert(option);
+    e.target.elements.option.value = '';
+   }    
+  };
   render() {
     return (
       <div>
-      AddOption component here 
+      <form onSubmit = {this.handleAddOption}>
+      <input type='text' name='option' />
+      <button>Submit</button>
+      </form>
       </div>
     );
   }
