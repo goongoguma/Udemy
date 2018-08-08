cf4051d92b427601e654300ca990ae5b62ad5715
Jae Hyun An <38463426+goongoguma@users.noreply.github.com>
2018-08-08 14:56:17 +0900

    Complete React
    Default Prop Values

--- Complete React/indecision-app/public/src/counter-example.js
+++ Complete React/indecision-app/public/src/counter-example.js
@@ -1,65 +1,53 @@
+// count - setup default prop value to 0
+
 class Counter extends React.Component {
 constructor(props) {
   super(props);
   this.handleAddOne = this.handleAddOne.bind(this);
   this.handleMinusOne = this.handleMinusOne.bind(this);
   this.handleReset = this.handleReset.bind(this);
+  this.state = {
+    count: props.count // count passed down a way to configure the default state down way the bottom 
+  };
  };
 handleAddOne() {
-  console.log('handleAddOne');
+ this.setState((prev) => { //prev is a state object before the changes have been applied
+   return {
+    count: prev.count + 1 //.count is current count value
+   };
+ }); // setState allows us to manupulate the object after that the component will be rendered automatically
  }
 handleMinusOne() {
-  console.log('handleMinusOne');
+  // call this.setState decrement the count by 1
+  this.setState((prev) => {
+    return {
+      count: prev.count - 1
+    };
+  });
  }
 handleReset() {
-   console.log('handleReset');
- }
+   this.setState(() => {
+     return {
+       count: 0
+     }
+   });
+  }
 render() {
   return(
     <div>
-    <h1>Count: </h1>
+    <h1>Count: {this.state.count}</h1>
     <button onClick={this.handleAddOne}>+1</button>
-      <button onClick={this.handleMinusOne}>-1</button>
-      <button onClick={this.handleReset}>reset</button>
+    <button onClick={this.handleMinusOne}>-1</button>
+    <button onClick={this.handleReset}>reset</button>
     </div>
   );
  }
 }
 
-
+Counter.defaultProps = {
+  count: 0
+}
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
 
 
-// let count = 0;
-// const addOne = () => {
-//   count++;
-//   renderCounterApp();
-// }
-// const minusOne = () => {
-//   // subtract 1 from count - rerender
-//   count--;
-//   renderCounterApp();
-// }
-// const reset = () => {
-//   count = 0;
-//   renderCounterApp();
-//   // set count to 0 a rerender
-// }
-
-// const appRoot = document.getElementById('app');
-
-// const renderCounterApp = () => {
-//   const templateTwo = (
-//     <div>
-//       <h1>Count: {count}</h1>
-//       <button onClick={addOne}>+1</button> 
-//       <button onClick={minusOne}>-1</button> 
-//       <button onClick={reset}>reset</button>
-//     </div>
-//   );
-
-//   ReactDOM.render(templateTwo, appRoot); // This is where we are gonna render out application
-// };
-
-// renderCounterApp();
