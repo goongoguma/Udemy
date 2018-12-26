5762ae06cb7628c91c914c53b3dd341611e60528
goongoguma <wogus7an@gmail.com>
2018-12-26 13:33:40 +0900

    Function Basics

--- The-Modern-JavaScript-Bootcamp/functions/functions-101.js
+++ The-Modern-JavaScript-Bootcamp/functions/functions-101.js
@@ -1,1 +1,35 @@
-// Function - input, code, output
+// Function - input(argument), code, output(return value)
+
+let greetUser = function() {
+  console.log("Welcome user!");
+};
+
+greetUser();
+greetUser();
+greetUser();
+
+let square = function(num) {
+  let result = num * num;
+  return result;
+};
+
+let value = square(3);
+let otherValue = square(10);
+
+console.log(value);
+console.log(otherValue);
+
+// Challenge Area
+
+// Declare convertFahrenheitToCelsius
+let convertFahrenheitToCelsius = function(fahrenheit) {
+  return (fahrenheit - 32) / 1.8;
+};
+
+// Call a couple of times (32 -> 0) (68 -> 20)
+let celcius1 = convertFahrenheitToCelsius(32);
+let celcius2 = convertFahrenheitToCelsius(68);
+
+// print the converted values
+console.log(celcius1); // 0
+console.log(celcius2); // 20
