79dfbd481540f297b40f2f8a5098306f359af997
goongoguma <wogus7an@gmail.com>
2018-12-26 09:50:06 +0900

    Strings and Variables

---
+++ The-Modern-JavaScript-Bootcamp/basics/strings.js
@@ -0,0 +1,13 @@
+let name = "Andrew Mead";
+console.log(name); // Andrew Mead
+
+let firstName = "Andrew"; // camelCase
+let lastName = "Mead";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName); // Andrew Mead
+
+let city = "Incheon";
+let country = "Republic of Korea";
+let location = city + ", " + country;
+console.log(location);
