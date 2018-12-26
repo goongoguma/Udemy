e4a94c7bd9560ed877eb78ae51cf575c239295ee
goongoguma <wogus7an@gmail.com>
2018-12-26 10:28:36 +0900

    simple temp-conversion exercise

---
+++ The-Modern-JavaScript-Bootcamp/basics/temp-conversion.js
@@ -0,0 +1,11 @@
+let fahrenheit = 32; // 0c - 273.15k
+
+// calculate celsiuse and store in celsius variable
+// print that value
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(celsius); // 0
+
+// calculate kelvin value and store in variable
+// print that value
+let kelvin = ((fahrenheit - 32) * 5) / 9 + 273.15;
+console.log(kelvin); // 273.15
