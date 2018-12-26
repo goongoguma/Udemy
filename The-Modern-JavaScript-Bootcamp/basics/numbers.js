2d11360bdd546755c746057deb602889f317fa57
goongoguma <wogus7an@gmail.com>
2018-12-26 10:06:00 +0900

    Numbers

---
+++ The-Modern-JavaScript-Bootcamp/basics/numbers.js
@@ -0,0 +1,19 @@
+let x = 11;
+// let num = x + 1 * 2; // 13
+let num = (x + 1) * 2;
+console.log(num); // 24
+
+let age = 29;
+let dogYears = (age + 1) / 7;
+console.log(dogYears); // 4.28
+
+// Challenge area
+
+// studentScore
+let studentScore = 18;
+// maxScore
+let maxScore = 20;
+// percent
+let percent = (studentScore / maxScore) * 100;
+// print the percent
+console.log(percent); // 90
