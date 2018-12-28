c37806405c0b93564d595a60dc308820c5d82c37
goongoguma <wogus7an@gmail.com>
2018-12-28 09:51:58 +0900

    Build a Grade Calculator

---
+++ The-Modern-JavaScript-Bootcamp/functions/grade-calc.js
@@ -0,0 +1,24 @@
+// two arguments : students score, total possible score
+// 15/20 -> print "You got a C (75%)!"
+// A: 90 - 100, B: 80 - 89, C: 70 - 79, D: 60 - 69, F: 0 - 59
+
+let grade = function(studentsScore, total) {
+  let percent = (studentsScore / total) * 100;
+  let letterGrade = "";
+  if (percent >= 90) {
+    letterGrade = "A";
+  } else if (percent >= 80) {
+    letterGrade = "B";
+  } else if (percent >= 70) {
+    letterGrade = "C";
+  } else if (percent >= 60) {
+    letterGrade = "D";
+  } else {
+    letterGrade = "F";
+  }
+  return `You got a ${letterGrade} (${percent}%)!`;
+};
+
+let gradeCalc = grade(13, 20);
+
+console.log(gradeCalc);
