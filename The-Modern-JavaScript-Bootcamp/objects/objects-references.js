936e06d012a53ad7407a7ed18048631095c863e4
goongoguma <wogus7an@gmail.com>
2018-12-28 11:12:29 +0900

    objects-references

---
+++ The-Modern-JavaScript-Bootcamp/objects/objects-references.js
@@ -0,0 +1,21 @@
+let myAccount = {
+  name: "Jae Hyun An",
+  expenses: 0,
+  income: 0
+};
+
+let otherAccount = myAccount;
+otherAccount.income = 10000; // it changes the property of myAccount
+otherAccount = {}; // binding is broken.
+
+// when the value of reference changes, the value of object also changes
+let addExpense = function(account, amount) {
+  // when we assign a new value to the argument, the binding between arguments and object is broken completely
+  // which means it can only manipulate properties in the object
+  account = {}; // {}
+  // account.expenses = account.expenses + amount; // { name: 'Jae Hyun An', expenses: 1500, income: 10000 }
+  console.log(account);
+};
+
+addExpense(myAccount, 1500);
+console.log(myAccount);
