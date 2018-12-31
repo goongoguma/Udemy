1547392987a117a6b4920ddf1e7ccd52f1b295ba
goongoguma <wogus7an@gmail.com>
2018-12-31 12:07:26 +0900

    Improve Our Expense Tracker

---
+++ The-Modern-JavaScript-Bootcamp/arrays/expense-tracker.js
@@ -0,0 +1,37 @@
+const account = {
+  name: "Jae Hyun",
+  expenses: [],
+  incomes: [],
+  addExpense: function(description, amount) {
+    this.expenses.push({ description: description, amount: amount });
+  },
+  addIncome: function(description, amount) {
+    this.incomes.push({ description: description, amount: amount });
+  },
+  getAccountSummary: function() {
+    let totalExpenses = 0;
+    let totalIncome = 0;
+    this.expenses.forEach(function(expense) {
+      totalExpenses += expense.amount;
+    });
+    this.incomes.forEach(function(income) {
+      totalIncome += income.amount;
+    });
+    return `${this.name} has a balance of $${totalIncome -
+      totalExpenses}. $${totalIncome} in income. $${totalExpenses} in expenses.`;
+  }
+};
+
+// object
+// Expense -> description, amount,
+//method
+// addExpense -> description, amount
+// getAccountSummary -> total up all expenses -> Jae Hyun has $952 in expenses.
+// add income array to account
+// addIncome method -> description, amount
+// Tweak getAccountSummary  -> Jae Hyun has a balance of $10. $100 in income. $90 in expenses.
+
+account.addExpense("Rent", 950);
+account.addExpense("Coffee", 2);
+account.addIncome("Job", 1000);
+console.log(account.getAccountSummary()); // Jae Hyun has $1250 in expenses.
