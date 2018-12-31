ffd354cbc46678a64037c16e5f3f518d59ff7c0d
goongoguma <wogus7an@gmail.com>
2018-12-31 13:45:36 +0900

    DOM Manipulation

--- The-Modern-JavaScript-Bootcamp/notes-app/notes-app.js
+++ The-Modern-JavaScript-Bootcamp/notes-app/notes-app.js
@@ -1,1 +1,15 @@
-console.log("This is from a different file");
+// DOM - Document Object Model
+
+// // query and remove
+// // querySelector finds the first match
+// const p = document.querySelector("p");
+// p.remove();
+
+//query all and remove
+const ps = document.querySelectorAll("p");
+
+ps.forEach(function(p) {
+  // console.log(p.textContent); // read comment
+  // p.remove();
+  p.textContent = "*****";
+});
