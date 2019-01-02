c2f4f1d2d458a65a8a846511ecfc8f08a5086ace
goongoguma <wogus7an@gmail.com>
2019-01-02 16:11:09 +0900

    The Edit Note Page : Part2

---
+++ The-Modern-JavaScript-Bootcamp/notes-app/notes-edit.js
@@ -0,0 +1,32 @@
+const titleEl = document.querySelector("#note-title");
+const bodyEl = document.querySelector("#note-body");
+const removeEl = document.querySelector("#remove-note");
+const noteId = location.hash.substring(1);
+const notes = getSavedNotes();
+const note = notes.find(function(note) {
+  return note.id === noteId;
+});
+
+// IN CASE ID IS WRONG, RELOCATE TO MAIN PAGE
+if (note === undefined) {
+  location.assign("/index.html");
+}
+
+titleEl.value = note.title;
+bodyEl.value = note.body;
+
+titleEl.addEventListener("input", function(e) {
+  note.title = e.target.value;
+  saveNotes(notes);
+});
+
+bodyEl.addEventListener("input", function(e) {
+  note.body = e.target.value;
+  saveNotes(notes);
+});
+
+removeEl.addEventListener("click", function() {
+  removeNote(note.id);
+  saveNotes(notes);
+  location.assign("/index.html");
+});
