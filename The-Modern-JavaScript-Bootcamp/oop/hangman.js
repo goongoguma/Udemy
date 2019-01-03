328c551180751611bf3e9a00beb121d4d72687dc
goongoguma <wogus7an@gmail.com>
2019-01-03 17:25:48 +0900

    Hangman Challenge : Part 1

--- The-Modern-JavaScript-Bootcamp/oop/hangman.js
+++ The-Modern-JavaScript-Bootcamp/oop/hangman.js
@@ -1,10 +1,25 @@
-const Hangman = function(word, remaining) {
-  this.word = word;
+const Hangman = function(word, remaining, guessedLetters) {
+  this.word = word.toLowerCase().split("");
   this.remaining = remaining;
+  this.guessedLetters = ["c", "e"];
+};
+
+Hangman.prototype.getPuzzle = function() {
+  let puzzle = "";
+
+  this.word.forEach(letter => {
+    if (this.guessedLetters.includes(letter) || letter === " ") {
+      puzzle += letter;
+    } else {
+      puzzle += "*";
+    }
+  });
+
+  return puzzle;
 };
 
 const game1 = new Hangman("cat", 2);
-console.log(game1);
+console.log(game1.getPuzzle());
 
 const game2 = new Hangman("hello", 4);
-console.log(game2);
+console.log(game2.getPuzzle());
