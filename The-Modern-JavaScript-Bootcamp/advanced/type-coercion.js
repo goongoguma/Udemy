0ada1fac3b9f10b7ff3eda229e9cfce5139ab398
goongoguma <wogus7an@gmail.com>
2019-01-03 13:57:01 +0900

    Type Coercion

---
+++ The-Modern-JavaScript-Bootcamp/advanced/type-coercion.js
@@ -0,0 +1,9 @@
+// TYPE COERCION - A STRING, A NUMBER, A BOOLEAN
+// console.log("5" + 5);
+// console.log("5" - 5);
+// console.log(5 === "5"); // false
+
+const value = true + 12; // true has value of 1, false has value of 0
+const type = typeof 123;
+console.log(type);
+console.log(value);
