61d136c3ef957f707f6725ab79bf5b7ae739a9ff
goongoguma <wogus7an@gmail.com>
2019-01-04 17:10:35 +0900

    The Class Syntax

--- The-Modern-JavaScript-Bootcamp/hangman/hangman.js
+++ The-Modern-JavaScript-Bootcamp/hangman/hangman.js
@@ -1,37 +1,36 @@
-const Hangman = function(word, remainingGuesses) {
-  this.word = word.toLowerCase().split("");
-  this.remainingGuesses = remainingGuesses;
-  this.guessedLetters = [];
-};
+class Hangman {
+  constructor(word, remainingGuesses) {
+    this.word = word.toLowerCase().split("");
+    this.remainingGuesses = remainingGuesses;
+    this.guessedLetters = [];
+  }
 
-Hangman.prototype.getPuzzle = function() {
-  let puzzle = "";
-  this.word.forEach(letter => {
-    if (this.guessedLetters.includes(letter) || letter === " ") {
-      puzzle += letter;
-    } else {
-      puzzle += "*";
-    }
-  });
-  return puzzle;
-};
-
-//  1. word가 포함하고 있는 알파벳이며 중복되지 않을경우 guessedLetter 배열에 넣어주기
-//  2. word에 포함되지 않는 알파벳인 경우에는 remaining의 숫자에서 빼주기
-
-Hangman.prototype.guessing = function(alphabet) {
-  guess = alphabet.toLowerCase();
-  if (this.remainingGuesses > 0) {
-    if (!this.guessedLetters.includes(guess) && !this.word.includes(guess)) {
-      this.remainingGuesses -= 1;
-      if (this.remainingGuesses === 0) {
-        document.querySelector(
-          "#status"
-        ).textContent = `GAME OVER. The word was "${this.word.join("")}"`;
-        return null;
+  getPuzzle() {
+    let puzzle = "";
+    this.word.forEach(letter => {
+      if (this.guessedLetters.includes(letter) || letter === " ") {
+        puzzle += letter;
+      } else {
+        puzzle += "*";
       }
-    } else if (!this.guessedLetters.includes(guess)) {
-      this.guessedLetters.push(guess);
+    });
+    return puzzle;
+  }
+
+  guessing(alphabet) {
+    const guess = alphabet.toLowerCase();
+    if (this.remainingGuesses > 0) {
+      if (!this.guessedLetters.includes(guess) && !this.word.includes(guess)) {
+        this.remainingGuesses -= 1;
+        if (this.remainingGuesses === 0) {
+          document.querySelector(
+            "#status"
+          ).textContent = `GAME OVER. The word was "${this.word.join("")}"`;
+          return null;
+        }
+      } else if (!this.guessedLetters.includes(guess)) {
+        this.guessedLetters.push(guess);
+      }
     }
   }
-};
+}
--- The-Modern-JavaScript-Bootcamp/oop/person.js
+++ The-Modern-JavaScript-Bootcamp/oop/person.js
@@ -1,38 +1,34 @@
 // Prototypal Inheritance
 
-const Person = function(firstName, lastName, age, likes = []) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.likes = likes;
-};
+class Person {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.likes = likes;
+  }
+  getBio() {
+    let bio = `${this.firstName} is ${this.age}.`;
 
-// IN ES6, IT USES this VALUE THAT PARENT HAS
-Person.prototype.getBio = function() {
-  let bio = `${this.firstName} is ${this.age}.`;
+    this.likes.forEach(like => {
+      // this IS USING THE VALUE IN THE VALUE THAT ITS PARENT HAS
+      // IF WE WANT TO USE this IN CALLBACK FUNCTION, USE ARROW FUNCTION
+      bio += ` ${this.firstName} likes ${like}.`;
+    });
 
-  this.likes.forEach(like => {
-    // this IS USING THE VALUE IN THE VALUE THAT ITS PARENT HAS
-    // IF WE WANT TO USE this IN CALLBACK FUNCTION, USE ARROW FUNCTION
-    bio += ` ${this.firstName} likes ${like}.`;
-  });
-
-  return bio;
-};
-
-Person.prototype.setName = function(fullName) {
-  const names = fullName.split(" ");
-  this.firstName = names[0];
-  this.lastName = names[1];
-};
+    return bio;
+  }
+  setName(fullName) {
+    const names = fullName.split(" ");
+    this.firstName = names[0];
+    this.lastName = names[1];
+  }
+}
 
 // CONSTRUCTOR FUNCTION new Person()
 const me = new Person("Jay", "Hyun", 28, ["teaching", "biking"]);
-me.firstName = "Jen";
-
 me.setName("Alexis Turner");
 console.log(me.getBio());
 
 const person2 = new Person("Clancey", "Turner", 51);
-
 console.log(person2.getBio());
