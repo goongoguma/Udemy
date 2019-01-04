826686f09cf725df864e96e84e11d61c109e0ab5
goongoguma <wogus7an@gmail.com>
2019-01-04 18:04:04 +0900

    Creating Subclasses

--- The-Modern-JavaScript-Bootcamp/oop/person.js
+++ The-Modern-JavaScript-Bootcamp/oop/person.js
@@ -1,4 +1,5 @@
 // Prototypal Inheritance
+// myPerson --> Person.prototype --> Object.prototype --> null
 
 class Person {
   constructor(firstName, lastName, age, likes = []) {
@@ -25,10 +26,47 @@ class Person {
   }
 }
 
-// CONSTRUCTOR FUNCTION new Person()
-const me = new Person("Jay", "Hyun", 28, ["teaching", "biking"]);
-me.setName("Alexis Turner");
-console.log(me.getBio());
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes) {
+    super(firstName, lastName, age, likes);
+    this.position = position;
+  }
+  getBio() {
+    return `${this.firstName} ${this.lastName} is a ${this.position}.`;
+    // Jae is a teacher
+  }
+  getYearsLeft() {
+    return 65 - this.age;
+  }
+}
 
-const person2 = new Person("Clancey", "Turner", 51);
-console.log(person2.getBio());
+// const me = new Employee("Jay", "Hyun", 28, "teacher", ["teaching", "biking"]);
+
+// me.setName("Alexis Turner");
+// console.log(me.getBio());
+// console.log(me.getYearsLeft());
+
+// const person2 = new Person("Clancey", "Turner", 51);
+// console.log(person2.getBio());
+
+class Student extends Person {
+  constructor(firstName, grade) {
+    super(firstName);
+    this.grade = grade;
+  }
+  upgradeGrade(change) {
+    this.grade += change;
+    return this.grade;
+  }
+  gradeResult() {
+    if (this.grade >= 70) {
+      return `${this.firstName} is passing the class`;
+    } else {
+      return `${this.firstName} is not passing the calss`;
+    }
+  }
+}
+
+const me = new Student("Jae", 85);
+console.log(me.upgradeGrade(10));
+console.log(me.gradeResult());
