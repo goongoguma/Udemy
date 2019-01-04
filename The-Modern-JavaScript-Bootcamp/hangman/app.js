65a3e854ba34bd15d2fef2b2675d8e43cc429698
goongoguma <wogus7an@gmail.com>
2019-01-04 10:39:25 +0900

    Primitives and Objects: Part2

--- The-Modern-JavaScript-Bootcamp/hangman/app.js
+++ The-Modern-JavaScript-Bootcamp/hangman/app.js
@@ -1,13 +1,17 @@
-// product --> Object.prototype --> null
-const product = {
-  name: "Influence"
-};
-// const product = new Object({
-//   name: "The war Of Art"
-// });
+// PRIMITIVE VALUE : STRING, NUMBER, BOOLEAN, NULL, UNDEFINED
 
-Object.prototype.someNewMethod = () => "This is the new function";
+// Object : myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunc --> Function.prototype --> Object.prototype --> null
 
-// hasOwnProperty
-console.log(product.someNewMethod());
+// WHEN WE TRY TO ACCESS A METHOD ON PRIMITIVE VALUE, IT CONVERTS INTO AN OBJECT AND THEN CALL METHOD ON THAT
+// String: myString --> String.prtotype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Obejct.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+
+const product = "Computer";
 console.log(product);
+
+const otherProduct = new String("Phone");
+
+console.log(otherProduct);
