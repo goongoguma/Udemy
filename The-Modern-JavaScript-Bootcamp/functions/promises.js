adcc059cb7cac726d01b3ac5fa02f7d9f1fb5874
goongoguma <wogus7an@gmail.com>
2019-01-09 16:59:17 +0900

    Exploring Promises

---
+++ The-Modern-JavaScript-Bootcamp/functions/promises.js
@@ -0,0 +1,31 @@
+// Callback
+const getDataCallback = callback => {
+  setTimeout(() => {
+    callback(undefined, "This is the data");
+  }, 2000);
+};
+
+getDataCallback((err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
+// Promise
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("this is the promise data");
+    reject("This is my promise error");
+  }, 2000);
+});
+
+myPromise.then(
+  data => {
+    console.log(data);
+  },
+  err => {
+    console.log(err);
+  }
+);
