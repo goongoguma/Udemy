37a9610c7e6ca580138b2f832c4b79cd364f1f62
goongoguma <wogus7an@gmail.com>
2019-01-09 15:05:25 +0900

    Callback Abstraction Challenge

--- The-Modern-JavaScript-Bootcamp/hangman/app.js
+++ The-Modern-JavaScript-Bootcamp/hangman/app.js
@@ -27,37 +27,25 @@ window.addEventListener("keypress", e => {
   remainingEl.textContent = `Remaining Guess : ${game1.remainingGuesses}`;
 });
 
-// getPuzzle(puzzle => {
-//   if (puzzle) {
-//     console.log(puzzle);
-//   } else {
-//     console.log(`Error : ${puzzle}`);
-//   }
-// });
+getPuzzle("2", puzzle => {
+  if (puzzle) {
+    console.log(puzzle);
+  } else {
+    console.log(`Error : ${puzzle}`);
+  }
+});
 
-// // SYNC EXAMPLE
-// const puzzle = getPuzzleSync();
-// console.log(puzzle);
-// console.log("do something else");
+// CHALLENGE
 
-// // CHALLENGE
-// // 1. MAKE A NEW REQUEST FOR ALL COUNTRIES
-// const countryRequest = new XMLHttpRequest();
-// countryRequest.open("GET", "http://restcountries.eu/rest/v2/all");
-// countryRequest.send();
-// const countryCode = "KR";
-// // 2. PARSE THE RESPONSETEXT TO GET BACK THE ARRAY OF OBJECTS
-// countryRequest.addEventListener("readystatechange", e => {
-//   if (e.target.readyState === 4 && e.target.status === 200) {
-//     const countries = JSON.parse(e.target.responseText);
-//     countries.forEach(country => {
-//       // 3. FIND YOUR COUNTRY OBJECT BY IT'S COUNTRY (ALPHA2CODE PROPERTY)
-//       if (country.alpha2Code === countryCode) {
-//         // 4. PRINT THE FULL COUNTRY NAME (NAME PROPERTY)
-//         console.log(country.name);
-//       } else if (e.target.readyState === 4) {
-//         console.log("Unable to fetch data");
-//       }
-//     });
-//   }
-// });
+// 1. Create a new funciton for getting country details
+// 2. Call it with two arguments : country code, the callback function
+// 3. Make the HTTP request and call the callback with country information
+// 4. Use the callback to print the country name
+
+getCountry("KR", whatCountry => {
+  if (whatCountry) {
+    console.log(whatCountry.name);
+  } else {
+    console.log("ERROR!!");
+  }
+});
--- The-Modern-JavaScript-Bootcamp/hangman/request.js
+++ The-Modern-JavaScript-Bootcamp/hangman/request.js
@@ -1,8 +1,8 @@
-const getPuzzle = callback => {
+const getPuzzle = (wordCount, callback) => {
   // MAKING AN HTTP REQUEST
   const request = new XMLHttpRequest();
   // 1. xhr 객체를 만든 후 open 메소드로 요청을 열고 주소와 HTTP 메소드를 설정한 뒤, send 메소드로 요청을 서버로 보낸다.
-  request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3");
+  request.open("GET", `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
   request.send();
   // this makes event fires when we have an information from server
   // 2. send 메소드로 요청을 서버에 보낸 뒤에 받은 정보를 이벤트리스너를 활용해 이벤트를 실행시킨다.
@@ -16,20 +16,19 @@ const getPuzzle = callback => {
   });
 };
 
-//  SYNC EXAMPLE
-const getPuzzleSync = () => {
-  // MAKING AN HTTP REQUEST
-  const request = new XMLHttpRequest();
-  // 1. xhr 객체를 만든 후 open 메소드로 요청을 열고 주소와 HTTP 메소드를 설정한 뒤, send 메소드로 요청을 서버로 보낸다.
-  request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3", false);
-  request.send();
-  // this makes event fires when we have an information from server
-  // 2. send 메소드로 요청을 서버에 보낸 뒤에 받은 정보를 이벤트리스너를 활용해 이벤트를 실행시킨다.
-
-  if (request.readyState === 4 && request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    return data.puzzle;
-  } else if (request.readyState === 4) {
-    throw new Error("Things did not go well");
-  }
+const getCountry = (countryCode, callback) => {
+  const countryRequest = new XMLHttpRequest();
+  countryRequest.open("GET", "http://restcountries.eu/rest/v2/all");
+  countryRequest.send();
+  countryRequest.addEventListener("readystatechange", e => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+      const countries = JSON.parse(e.target.responseText);
+      const myCountry = countries.find(
+        country => country.alpha2Code === countryCode
+      );
+      callback(myCountry);
+    } else if (e.target.readyState === 4) {
+      callback(undefined);
+    }
+  });
 };
