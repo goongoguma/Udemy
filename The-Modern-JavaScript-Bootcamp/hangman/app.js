316ab7d24e4be05d43d598302d20bcef84be74a0
goongoguma <wogus7an@gmail.com>
2019-01-10 12:38:47 +0900

    Integrating Data into the Application

--- The-Modern-JavaScript-Bootcamp/hangman/app.js
+++ The-Modern-JavaScript-Bootcamp/hangman/app.js
@@ -4,10 +4,8 @@
 const startEl = document.querySelector("#start");
 const wordEl = document.querySelector("#word");
 const remainingEl = document.querySelector("#remaining");
-const game1 = new Hangman("cat", 3);
-
-wordEl.textContent = game1.getPuzzle(); // c*t
-remainingEl.textContent = `Remaining Guess : ${game1.remainingGuesses}`;
+const pEl = document.querySelector("#status");
+let game1;
 
 window.addEventListener("keypress", e => {
   const guess = String.fromCharCode(e.charCode);
@@ -17,39 +15,44 @@ window.addEventListener("keypress", e => {
     document.querySelector("#status").textContent = "PLAYING...";
   }
   game1.guessing(guess);
-
-  wordEl.textContent = game1.getPuzzle(); // c*t
+  render();
 
   if (!wordEl.textContent.split("").includes("*")) {
     document.querySelector("#status").textContent = "YOU GOT IT RIGHT!";
   }
-
-  remainingEl.textContent = `Remaining Guess : ${game1.remainingGuesses}`;
 });
 
-getPuzzle("2")
-  .then(puzzle => {
-    console.log(puzzle);
-  })
-  .catch(err => {
-    console.log(`Error: ${err}`);
-  });
+const render = () => {
+  wordEl.textContent = game1.getPuzzle(); // c*t
+  remainingEl.textContent = `Remaining Guess : ${game1.remainingGuesses}`;
+};
 
-// getLocation()
-//   .then(location => {
-//     return getCountry(location.country);
-//   })
-//   .then(resolvedCountry => {
-//     console.log(resolvedCountry);
+const startGame = async () => {
+  const puzzle = await getPuzzle("2");
+  game1 = new Hangman(puzzle, 5);
+  console.log(game1);
+  render();
+};
+
+document.querySelector("#reset").addEventListener("click", () => {
+  startGame();
+  pEl.textContent = "";
+});
+
+startGame();
+
+// getPuzzle("2")
+//   .then(puzzle => {
+//     console.log(puzzle);
 //   })
 //   .catch(err => {
 //     console.log(`Error: ${err}`);
 //   });
 
-getCurrentCountry()
-  .then(country => {
-    console.log(country);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// getCurrentCountry()
+//   .then(country => {
+//     console.log(country);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
