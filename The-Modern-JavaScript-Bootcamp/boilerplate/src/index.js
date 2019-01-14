3cc1f5d5dba5466e087de0dc49bb900ab5279a30
goongoguma <wogus7an@gmail.com>
2019-01-14 17:16:37 +0900

    JavaScript Moduels : Part 2 (named export, default export)

--- The-Modern-JavaScript-Bootcamp/boilerplate/src/index.js
+++ The-Modern-JavaScript-Bootcamp/boilerplate/src/index.js
@@ -1,7 +1,10 @@
-import { add, name } from "./utilities";
-import { scream } from "./scream";
+import otherSquare, { add, name } from "./utilities";
+import otherScream from "./scream";
+
+// 기능 하나가 담긴 함수를 export 할때는 namedExport가, 기능이 여러개인 함수 여러개를 export 할때는 defaultExport를 쓰는게 좋다.
 
 console.log("index.js");
 console.log(add(3, 2));
 console.log(name);
-console.log(scream("hello world"));
+console.log(otherSquare(10));
+console.log(otherScream("hello world"));
