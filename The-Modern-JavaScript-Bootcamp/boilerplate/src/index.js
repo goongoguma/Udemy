080bf43940294c22deee64f876669975e211fd2d
goongoguma <wogus7an@gmail.com>
2019-01-15 17:26:45 +0900

    The Object Spread Syntax

--- The-Modern-JavaScript-Bootcamp/boilerplate/src/index.js
+++ The-Modern-JavaScript-Bootcamp/boilerplate/src/index.js
@@ -14,3 +14,40 @@ printTeam(team.name, team.coach, ...team.players);
 const cities = ["NewYork", "Seoul", "Tokyo"];
 
 const citiesCopy = ["Santiago", ...cities];
+
+// The Object Spread Syntax
+let house = {
+  bedrooms: 2,
+  bathrooms: 1.5,
+  yearBuilt: 2017
+};
+
+let newHouse = {
+  // we can set a new value for the object
+  basement: true,
+  // all of the properties from house will copy over this object
+  ...house
+};
+
+newHouse.yearBuilt = 2018;
+
+console.log(house);
+console.log(newHouse);
+
+// Object Spread Syntax Challenge
+const person = {
+  name: "Jay",
+  age: 25
+};
+
+const location = {
+  city: "Incheon",
+  country: "S.Korea"
+};
+
+const overview = {
+  ...person,
+  ...location
+};
+
+console.log(overview);
