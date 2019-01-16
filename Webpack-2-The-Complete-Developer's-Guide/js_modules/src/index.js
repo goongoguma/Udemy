75598992a2fd89911bbec020c23bedcef7ead422
goongoguma <wogus7an@gmail.com>
2019-01-16 10:12:36 +0900

    Linking Files with CommonJS

--- Webpack-2-The-Complete-Developer's-Guide/js_modules/src/index.js
+++ Webpack-2-The-Complete-Developer's-Guide/js_modules/src/index.js
@@ -1,1 +1,6 @@
 // Calls functions from sum.js, then prints the result
+const sum = require("./sum");
+
+const total = sum(10, 5);
+
+console.log(total);
--- Webpack-2-The-Complete-Developer's-Guide/js_modules/src/sum.js
+++ Webpack-2-The-Complete-Developer's-Guide/js_modules/src/sum.js
@@ -1,2 +1,4 @@
 // Utility functions for working with mathmatical calculations
 const sum = (a, b) => a + b;
+
+module.exports = sum;
