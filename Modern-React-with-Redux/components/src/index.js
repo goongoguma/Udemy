e7ede4c96990debb68117cb6c0ed41e8bcd3a5a1
goongoguma <wogus7an@gmail.com>
2019-01-18 16:49:43 +0900

    Passing Props - Solutions

--- Modern-React-with-Redux/components/src/index.js
+++ Modern-React-with-Redux/components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:00PM"
+        commentText="Thank you!"
+        source={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 9:00PM"
+        commentText="I like the post"
+        source={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 1:00PM"
+        commentText="It helps me, thanks :)"
+        source={faker.image.avatar()}
+      />
     </div>
   );
 };
