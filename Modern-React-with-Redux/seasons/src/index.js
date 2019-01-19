f31ba8540147ca58618261a6304b05613eafc664
goongoguma <wogus7an@gmail.com>
2019-01-19 12:26:48 +0900

    Conditionally Rendering Content

--- Modern-React-with-Redux/seasons/src/index.js
+++ Modern-React-with-Redux/seasons/src/index.js
@@ -24,12 +24,13 @@ class App extends React.Component {
   }
   // React syas we have to define render!!
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat} <br />
-        Error:{this.state.errorMessage}
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    } else {
+      return <div>Loading...</div>;
+    }
   }
 }
 
