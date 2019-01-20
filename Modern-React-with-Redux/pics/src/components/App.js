7fa2a1d244834cbc0e5f6e80128a5320b1e1609f
goongoguma <wogus7an@gmail.com>
2019-01-20 16:02:45 +0900

    Viewing Request Results

--- Modern-React-with-Redux/pics/src/components/App.js
+++ Modern-React-with-Redux/pics/src/components/App.js
@@ -3,7 +3,17 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {}
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      // It specifies the different query string parameters we want to add into the request
+      // It is same as ?query=cars at the end of the address
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 88edb644808dce2dd69b18699b97d02efc2a6e881138304c45c9483cd8440bd2"
+      }
+    });
+  }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
