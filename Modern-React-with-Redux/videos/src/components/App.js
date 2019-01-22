2ca83507b8e3c9f3547ab81bdf9505ad776ccec2
goongoguma <wogus7an@gmail.com>
2019-01-22 16:00:28 +0900

    Passing State as Props

--- Modern-React-with-Redux/videos/src/components/App.js
+++ Modern-React-with-Redux/videos/src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
   state = { videos: [] };
+
   onTermSubmit = async term => {
     const res = await youtube.get("/search", {
       params: {
@@ -19,8 +21,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
-        {this.state.videos.length} videos.
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
