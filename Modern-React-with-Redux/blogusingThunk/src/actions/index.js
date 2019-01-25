f74d4062a7b30d989ef20fbb4e2f0c68147536f3
goongoguma <wogus7an@gmail.com>
2019-01-25 18:29:00 +0900

    Fetching Singular Records

--- Modern-React-with-Redux/blogusingThunk/src/actions/index.js
+++ Modern-React-with-Redux/blogusingThunk/src/actions/index.js
@@ -7,3 +7,11 @@ export const fetchPosts = () => {
     dispatch({ type: "FETCH_POSTS", payload: res.data });
   };
 };
+
+export const fetchUser = id => {
+  return async (dispatch, getState) => {
+    const res = await jsonPlaceholder.get(`/user/${id}`);
+
+    dispatch({ stype: "FETCH_USER", payload: res.data });
+  };
+};
