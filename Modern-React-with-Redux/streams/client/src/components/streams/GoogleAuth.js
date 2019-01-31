dda3ed8299979394bdba185233de66a3a9a7714f
goongoguma <wogus7an@gmail.com>
2019-01-31 12:25:30 +0900

    On-Demand Sign In and Sign Out

--- Modern-React-with-Redux/streams/client/src/components/streams/GoogleAuth.js
+++ Modern-React-with-Redux/streams/client/src/components/streams/GoogleAuth.js
@@ -26,19 +26,27 @@ class GoogleAuth extends React.Component {
     });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button className="ui red google button" onClick={this.onSignOut}>
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui red google button">
+        <button className="ui red google button" onClick={this.onSignIn}>
           <i className="google icon" />
           Sign In with Google
         </button>
