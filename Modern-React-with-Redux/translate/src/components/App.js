759471692ba31a893fca29a172e7f3947f715de6
goongoguma <wogus7an@gmail.com>
2019-02-18 10:48:41 +0900

    Selecting a Language

--- Modern-React-with-Redux/translate/src/components/App.js
+++ Modern-React-with-Redux/translate/src/components/App.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 class App extends React.Component {
+  state = { language: "English" };
+
+  onLanguageChange = language => {
+    this.setState({
+      language
+    });
+  };
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        Select a language:
+        <i
+          className="flag us"
+          onClick={() => this.onLanguageChange("English")}
+        />
+        <i
+          className="flag kr"
+          onClick={() => this.onLanguageChange("한국어")}
+        />
+        <div>{this.state.language}</div>
+      </div>
+    );
   }
 }
 
